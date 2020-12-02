fee8dcdf5f489ef2bb1178a520373b70aa12cea3
seumsims <seumworldinbangladesh@gmail.com>
2020-12-02 16:56:13 +0600

    1 file modified

--- src/components/Mytable.js
+++ src/components/Mytable.js
@@ -3,13 +3,20 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 class Mytable extends Component {
+
   render() {
+
+    
+  const col = [{
+    
+  }]
+  
     return (
       <div className="container">
         <ReactTable
         data = {}
         columns =  {}
-        defaultPageSize = {}
+        defaultPageSize = {2}
         pageSizeOptions = {[2,4,6,8,10]}
          />
       </div>
